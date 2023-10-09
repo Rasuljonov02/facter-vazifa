c96daad7a7091ea254fba1b9de5109f5a9089fff
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-09 11:54:41 +0500

    f

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-// import "./stlye.css";
-import { faker } from '@faker-js/faker';
+import "./main.css";
+import { faker } from "@faker-js/faker";
 
+const studmalumot: HTMLButtonElement = document.querySelector(".student")!;
+
+const contener: HTMLDivElement = document.querySelector(".contener")!;
 
 const randomName = faker.person.fullName();
 const randomEmail = faker.internet.email();
-console.log(randomName);
-console.log(randomEmail);
 
+studmalumot.addEventListener("click", () => {
+	const pelementname = document.createElement("p");
+	pelementname.classList.add("malumot");
+	pelementname.innerText = ` name :${randomName}`;
+	contener.appendChild(pelementname);
 
+	const pelementemail = document.createElement("p");
+	pelementemail.classList.add("ma’lumot");
+	pelementemail.innerText = `Email :${randomEmail}`;
+	contener.appendChild(pelementemail);
+
+	console.log(randomName);
+	console.log(randomEmail);
+	console.log("Button clicked!");
+});
