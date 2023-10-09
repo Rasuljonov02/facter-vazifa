f9aafbbbbe7004bdbdfbf78953fab38004ceb0d2
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-09 11:09:03 +0500

    f

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// import "./stlye.css";
+import { faker } from '@faker-js/faker';
+
+
+const randomName = faker.person.fullName();
+const randomEmail = faker.internet.email();
+console.log(randomName);
+console.log(randomEmail);
+
+
