3500fd511a653a9fbdb1336063c3f3844ebcf4fb
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-09 23:31:14 +0500

    as

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,60 @@
 import "./main.css";
 import { faker } from "@faker-js/faker";
+import * as yup from "yup";
 
 const btn: HTMLButtonElement = document.querySelector(".btn")!;
 const span: HTMLParagraphElement = document.querySelector(".span")!;
 const contener: HTMLDivElement = document.querySelector(".contener")!;
 const name: HTMLParagraphElement = document.querySelector(".name")!;
 const email: HTMLParagraphElement = document.querySelector(".email")!;
-const input: HTMLInputElement = document.querySelector("input")!;
+const input: HTMLInputElement = document.querySelector("#kasb")!;
 const xasb: HTMLParagraphElement = document.querySelector(".xasb")!;
 const biol: HTMLParagraphElement = document.querySelector(".biol")!;
+
+const schema = yup.object().shape({
+	name: yup
+		.string()
+		.matches(/^[A-Za-z\s]+$/, "Please enter only name ")
+		.required("Name is required"),
+});
+
 btn.addEventListener("click", (e) => {
+	const formData = {
+		name: input.value,
+	};
+	let s: number = 5;
+	schema
+		.validate(formData)
+		.then(() => {
+			const ll = setInterval(() => {
+				if (s === 0) {
+					input.className = "";
+					clearInterval(ll);
+				}
+				s--;
+			}, 1000);
+
+			input.className = "zor";
+		})
+		.catch((error) => {
+
+			const ll = setInterval(() => {
+				if (s === 0) {
+						input.className = "";
+						clearInterval(ll);
+				}
+
+				s--;
+		}, 1000);
+			input.className = "hato";
+		});
 	const randomName = faker.person.fullName();
 	const randomEmail = faker.internet.email();
-	const randombio = faker.lorem.paragraphs();
+	const randomBio = faker.lorem.paragraphs();
 
 	xasb.innerText = input.value;
 	name.innerText = ` ${randomName}`;
-	biol.innerText = ` ${randombio}`;
+	biol.innerText = ` ${randomBio}`;
 	email.innerText = ` ${randomEmail}`;
 	span.style.display = "flex";
 	contener.style.gap = "20px";
